848a9d9d81dd49e044b7e86c7ab3a38c41ca643d
UKE12368044 <balaji.balasubramani@tesco.com>
2022-09-10 15:26:57 +0530

    Init upload

---
+++ src/main/java/com/dsa/OptimizerBoilerPlate/OptimizerBoilerPlateApplication.java
@@ -0,0 +1,14 @@
+package com.dsa.OptimizerBoilerPlate;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizerBoilerPlateApplication {
+
+	public static void main(String[] args) {
+
+		//SpringApplication.run(OptimizerBoilerPlateApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/dsa/OptimizerBoilerPlate/OptimizerBoilerPlateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsa.OptimizerBoilerPlate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimizerBoilerPlateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
